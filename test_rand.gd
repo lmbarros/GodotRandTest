d7ea0a906258a04859c5b96b47e339743aef508f
Leandro Motta Barros <lmb@stackedboxes.org>
2017-03-05 14:56:48 -0300

    Add tests for all missing agorithms

--- test_rand.gd
+++ test_rand.gd
@@ -19,7 +19,7 @@ func test_sequence(rng, the_seed, expected_values):
 		var actual = rng.random()
 		if actual != expected:
 			$LstOutput.add_item("")
-			$LstOutput.add_item("ERROR! Unexpected random sequence!")
+			$LstOutput.add_item("!!!! ERROR! Unexpected random sequence!")
 			$LstOutput.add_item("")
 			return
 
@@ -31,12 +31,8 @@ func _on_BtnKnuthLCG_pressed():
 
 	var rng = RandKnuthLCG.new()
 
-	test_sequence(rng, 112233, [14373087394460283212, 17919820627726294955,
-		2374928239783487326, 1355612529541287637, 14300717203320031168])
-
-	test_sequence(rng, 97531, [2475730772265627958, 16585264879776313805,
-	   	10829226592693777752, 4337668493995821511, 13619766792622649930])
-
+	test_sequence(rng, 112233, [14373087394460283212, 17919820627726294955, 2374928239783487326, 1355612529541287637, 14300717203320031168])
+	test_sequence(rng, 97531, [2475730772265627958, 16585264879776313805, 10829226592693777752, 4337668493995821511, 13619766792622649930])
 	test_distributions(rng)
 
 
@@ -47,10 +43,53 @@ func _on_BtnPCG32_pressed():
 
 	var rng = RandPCG32.new()
 
-	test_sequence(rng, 112233, [2742036491, 2245988297, 365294676, 2892959733,
-		268736847])
+	test_sequence(rng, 112233, [102148438, 2290673564, 2768362330, 564360816, 3408622802])
+	test_sequence(rng, 97531, [3442234210, 3111326070, 2927093750, 2463984830, 4287579801])
+	test_distributions(rng)
 
-	test_sequence(rng, 97531, [107914430, 376050961, 4272170875, 909441240,
-		2368777700])
 
+func _on_BtnSplitMix64_pressed():
+	$LstOutput.clear()
+	$LstOutput.add_item("---- RandSplitMix64 ----")
+	$LstOutput.add_item("")
+
+	var rng = RandSplitMix64.new()
+
+	test_sequence(rng, 112233, [17521931875799178301, 14383456051838709316, 2828063782273925862, 11956729851173419597, 15283991741788426049])
+	test_sequence(rng, 97531, [14309215551682402496, 13331760577475424929, 6476874722340802351, 13089128203191675115, 7925169073796594034])
+	test_distributions(rng)
+
+
+func _on_BtnXoroshiro128Plus_pressed():
+	$LstOutput.clear()
+	$LstOutput.add_item("---- RandXoroshiro128Plus ----")
+	$LstOutput.add_item("")
+
+	var rng = RandXoroshiro128Plus.new()
+
+	test_sequence(rng, 13579, [5883773771842788424, 9989605189133216712, 7811117801491353185, 4706773927854861091, 17379387784680010921])
+	test_distributions(rng)
+
+
+func _on_tnMT19937_pressed():
+	$LstOutput.clear()
+	$LstOutput.add_item("---- RandMT19937_64 ----")
+	$LstOutput.add_item("")
+
+	var rng = RandMT19937.new()
+
+	test_sequence(rng, 112233, [2749130340, 394058186, 424636858, 3006322354, 2662410484])
+	test_sequence(rng, 97531, [3873955303, 573781378, 3704291903, 1481365957, 2780134746])
+	test_distributions(rng)
+
+
+func _on_BtnMT19937_64_pressed():
+	$LstOutput.clear()
+	$LstOutput.add_item("---- RandMT19937_64 ----")
+	$LstOutput.add_item("")
+
+	var rng = RandMT19937_64.new()
+
+	test_sequence(rng, 112233, [18385898271542208877, 10388062775371028914, 6652856830127087739, 1890554855370499999, 14650261722016513395])
+	test_sequence(rng, 97531, [6788201074649841524, 11921711492735785818, 9983243477236236108, 5762306495090730538, 636925918925826639])
 	test_distributions(rng)
